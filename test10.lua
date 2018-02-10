0b54506a67773859bfcceadf68edad06fe7ccddc
Ning Jin“” <ningjin.unique@gmail.com>
2018-02-10 17:10:20 +0800

    Fix Chapter 10 exercise

--- test10.lua
+++ test10.lua
@@ -1,5 +1,12 @@
---[[ 
--- 10.1
+-- Chapter 10. Pattern Matching
+
+---[[ 
+-- 10.1: Write a function split that receives a string and a delimiter pattern and returns a sequence with the chunks in the original string separated by the delimiter:
+
+-- t = split("a whole new world", " ")
+-- -- t = {"a", "whole", "new", "world"}
+
+-- How does your function handle empty strings? (In particular, is an empty string an empty sequence or a sequence with one empty string?)
 
 function split(s, d)
     local words = {}
@@ -11,6 +18,7 @@ function split(s, d)
     return words
 end
 
+print("--- 10.1 ---")
 t = split("a%whole%new%%world", "%%")
 
 for _, v in ipairs(t) do print(v) end
@@ -18,8 +26,10 @@ for _, v in ipairs(t) do print(v) end
 --]]
 
 
---[[
--- 10.2
+---[[
+-- 10.2: The patterns '%D' and '[^%d]' are equivalent. What about the patterns '[^%d%u]' and '[%D%U]'?
+
+print("\n--- 10.2 ---")
 
 p1 = "%d"  -- digits
 p2 = "%D"  -- not digits
@@ -37,8 +47,8 @@ print(string.match("ABCDEFG123456abcdefg", p6))
 --]]
 
 
---[[
--- 10.3
+---[[
+-- 10.3:Write a function transliterate. This function receives a string and replaces each character in that string with another character, according to a table given as a second argument. If the table maps a to b, the function should replace any occurrence of a with b. If the table maps a to false, the function should remove occurrences of a from the resulting string.
 
 function transliterate(s, t)
     return (string.gsub(s, ".", function(c)
@@ -46,13 +56,18 @@ function transliterate(s, t)
     end))
 end
 
+print("\n--- 10.3 ---")
 print(transliterate("abcdefg12345", {a=1,b=2,c=3}))
 
 --]]
 
 
---[[
--- 10.4
+---[[
+-- 10.4: At the end of the section called “Captures”, we defined a trim function. Because of its use of backtracking, this function can take a quadratic time for some strings. (For instance, in my new machine, a match for a 100 KB string can take 52 seconds.)
+-- • Create a string that triggers this quadratic behavior in function trim.
+-- • Rewrite that function so that it always works in linear time
+
+print("\n--- 10.4 ---")
 
 f = io.open("test10.txt", "w")
 
@@ -94,11 +109,17 @@ ss = io.open("test10.txt", "r"):read('*all')
 t = os.clock()
 trim2(ss)
 print(string.format("time spent: %f", os.clock() - t))
+
 --]]
 
 
---[[
--- 10.5
+---[[
+-- 10.5:  Write a function to format a binary string as a literal in Lua, using the escape sequence \x for all bytes:
+
+-- print(escape("\0\1hello\200"))
+-- --> \x00\x01\x68\x65\x6C\x6C\x6F\xC8
+
+-- As an improved version, use also the escape sequence \z to break long lines.
 
 function escape(s)
     return (string.gsub(s, ".", function(c)
@@ -106,13 +127,15 @@ function escape(s)
     end))
 end
 
+print("\n--- 10.5 ---")
 print(escape("\0\1hello\200"))
 
 --]]
 
 
---[[
--- 10.6
+---[[
+-- 10.6: Rewrite the function transliterate for UTF-8 characters.
+
 -- utf8charpattern = '[\0-\x7F\xC2-\xF4][\x80-\xBF]*'
 -- utf8charpattern = '[\0-\127\194-\244][\128-\191]*'
 utf8charpattern = "([%z\1-\127\194-\244][\128-\191]*)"
@@ -129,13 +152,15 @@ function transliterate(s, t)
     end))
 end
 
+print("\n--- 10.6 ---")
 print(transliterate("尼玛比亚", {["尼"]=1,["玛"]=2,c=3}))
 
 --]]
 
 
---[[
--- 10.7
+---[[
+-- 10.7: Write a function to reverse a UTF-8 string.
+
 utf8charpattern = "([%z\1-\127\194-\244][\128-\191]*)"
 
 function reverse(s)
@@ -146,6 +171,7 @@ function reverse(s)
     return table.concat(words)
 end
 
+print("\n--- 10.7 ---")
 print(reverse("尼玛比亚"))
 
 --]]
