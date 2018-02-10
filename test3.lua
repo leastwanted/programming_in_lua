4eb70b91de8e856280409b29ccc8432747fd8902
Ning Jin“” <ningjin.unique@gmail.com>
2018-02-10 14:59:50 +0800

    Fix Chapter 3 Exercise

--- test3.lua
+++ test3.lua
@@ -1,49 +1,123 @@
-print(math.sin(math.pi / 2))
+-- Chapter 3. Numbers
 
-math.randomseed(os.time())
+---[[
+-- 3.1: Which of the following are valid numerals? What are their values?
+-- .0e12 .e12 0.0e 0x12 0xABFG 0xA FFFF 0xFFFFFFFF 0x 0x1P10 0.1e1 0x0.1p1
 
-print(math.random())
+print("--- 3.1 ---")
 
-print(math.random(6))
+print(.0e12)
 
-print(math.random(10, 20))
+-- print(.e12)
 
-print(math.modf(3.3))
+-- print(0.0e)
 
-print(math.maxinteger)
+print(0x12)
 
--- print(math.type(1)) -- not supported in lua 5.1
+-- print(0xABFG)
 
--- math.tointeger(-2.5)
+print(0xA)
 
--- 3.1
+-- print(FFFF)
 
--- a = 0x0.1p1
+print(0xFFFFFFFF)
 
-print(a)
+-- print(0x)
+
+print(0x1P10)
+
+print(0.1e1)
+
+print(0x0.1p1)
+
+--]]
 
 
--- 3.2
+---[[
+-- 3.2: Explain the following results:
 
--- 3.3
+-- math.maxinteger * 2
+-- math.mininteger * 2
+-- math.maxinteger * math.maxinteger
+-- math.mininteger * math.mininteger
+
+-- (Remember that integer arithmetic always wraps around.)
+
+print("\n--- 3.2 ---")
+
+print(math.maxinteger * 2)
+print(math.mininteger * 2)
+print(math.maxinteger * math.maxinteger)
+print(math.mininteger * math.mininteger)
+
+--]]
+
+
+---[[
+-- 3.3: What will the following program print?
+
+-- for i = -10, 10 do
+--     print(i, i % 3)
+-- end
+
+print("\n--- 3.3 ---")
 
 for i = -10, 10 do
-	print(i, i % 3)
+    print(i, i % 3)
 end
 
--- 3.4
+--]]
+
+
+---[[
+-- 3.4: What is the result of the expression 2^3^4? What about 2^-3^4?
+
+print("\n--- 3.4 ---")
 
 print(2^3^4)
-
 print(2^-3^4)
-print(2^(-3^4))
 
--- 3.5
+--]]
 
 
--- 3.6
+---[[
+-- 3.5: The number 12.7 is equal to the fraction 127/10, where the denominator is a power of ten. Can you express it as a common fraction where the denominator is a power of two? What about the number 5.5?
+
+-- 12.7 can not beexpressed as power of two
+
+-- 5.5 = 11 / 2
+
+--]]
+
+
+---[[
+-- 3.6: Write a function to compute the volume of a right circular cone, given its height and the angle between a generatrix and the axis.
 
 function calc_cone(h, a)
-	local r = math.tan(a) * h
-	return math.pi * r * r * h / 3
+    local r = math.tan(math.rad(a)) * h
+    return math.pi * r * r * h / 3
 end
+
+print("\n--- 3.6 ---")
+print(calc_cone(1, 45))
+
+--]]
+
+
+---[[
+-- 3.7: Using math.random, write a function to produce a pseudo-random number with a standard normal(Gaussian) distribution.
+
+function normal(mu, sigma)
+    mu = mu or 0
+    sigma = sigma or 1
+    local z0 = math.sqrt(-2.0 * math.log(math.random())) * math.cos(math.pi * 2.0 * math.random())
+    return z0 * sigma + mu
+
+end
+
+print("\n--- 3.7 ---")
+for i = 1, 10 do
+    print(normal())
+end
+
+--]]
