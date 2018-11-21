ac21abb125db8bdaa6235c3ca53bfc22851b0024
leastwanted <ningjin.unique@gmail.com>
2018-11-21 17:17:13 +0800

    add lua5.1.5 cmake

---
+++ lua-5.1.5/make.bat
@@ -0,0 +1,22 @@
+@echo off
+
+set GENERATOR=Visual Studio 12
+
+rem if not defined platform set platform=x64
+rem if "%platform%" EQU "x64" (set GENERATOR=%GENERATOR% Win64)
+
+IF NOT "x%1" == "x" GOTO :%1
+
+GOTO :build
+
+:build
+cmake -H. -Bbuild -G"%GENERATOR%"
+cmake --build build --config Release
+GOTO :end
+
+:clean
+IF EXIST build RMDIR /S /Q build
+IF EXIST lua.exe DEL /F /Q lua.exe
+GOTO :end
+
+:end
